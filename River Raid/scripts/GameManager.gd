cebb6772b5a8113c34cfe46df8c94712a54efb62
RafalWilkowski <kaizen.avc@gmail.com>
2019-11-19 11:52:11 +0100

    Change gamemanager

--- River Raid/scripts/GameManager.gd
+++ River Raid/scripts/GameManager.gd
@@ -1,40 +1,53 @@
 extends Node2D
 
+signal ready_to_go
+
 export var prestart_speed: float
 export var max_prestart_position_y: int
+export var min_reset_position_y: int
 var player: Node2D
 var starting_point : Vector2
 
 func _ready():
 	player = $Player
 	starting_point = $StartingPoint.position
+	connect("ready_to_go", player, "_on_ready_to_go")
 	yield(gameloop(), "completed")
 
 func gameloop():
-	print("gameloop")
-	yield(prestart(),"completed")
+	yield(start_game(),"completed")
+	yield(game_running(),"completed")
+	yield(gameover(),"completed")
 
-func prestart():
-	var nextframe: bool = false
+func start_game():
 	player.hide()
-	print("prestart")
 	while true:
-		player.position += Vector2(0,-1) * prestart_speed * get_process_delta_time()
+		auto_move()
 		yield(get_tree(),"idle_frame")
-		if player.position.y <= max_prestart_position_y && nextframe == false:
+		if player.position.y <= max_prestart_position_y:
 			#black screen animation fade and continue after finished
-			player.position.y = 1600
-			nextframe = true
-		if player.position.y <= starting_point.y && nextframe:
-			player.show()
-			yield(get_tree().create_timer(1.5),"timeout")
-			player.speed = 300
+			reset_game()
 			break
 
-func startgame():
-	pass
+func reset_game():
+	player.position.x = starting_point.x
+	player.position.y = min_reset_position_y
+
+	while true:
+		auto_move()
+		yield(get_tree(),"idle_frame")
+		if player.position.y <= starting_point.y:
+			player.show()
+			emit_signal("ready_to_go")
+			break
+
+func game_running():
+	yield(player, "no_more_lives")
+
 func gameover():
-	pass
-func resetgame():
-	pass
+	yield(get_tree(),"idle_frame")
+	gameloop()
+
+func auto_move():
+	player.position += Vector2(0,-1) * prestart_speed * get_process_delta_time()
 
