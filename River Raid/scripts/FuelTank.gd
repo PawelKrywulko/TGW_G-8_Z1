9688b006df371c637d1a9a5b8bcd42d0672be38f
pkrywulko <pkrywulko@gmail.com>
2019-11-20 19:14:41 +0100

    fuel tank fix

--- River Raid/scripts/FuelTank.gd
+++ River Raid/scripts/FuelTank.gd
@@ -3,6 +3,8 @@ extends Area2D
 signal player_refueled
 signal fuel_tank_destroyed
 
+export (int) var points = 50
+
 func _on_FuelTank_area_entered(area):
 	var area_name = area.get_name()
 	if area_name == "Player":
@@ -10,5 +12,5 @@ func _on_FuelTank_area_entered(area):
 		emit_signal("player_refueled")
 	if area_name == "Projectile":
 		queue_free()
-		print("fuel_tank_destroyed")
-		emit_signal("fuel_tank_destroyed", 50)
+		print("fuel_tank_destroyed; points: %s" % points)
+		emit_signal("fuel_tank_destroyed", points)
