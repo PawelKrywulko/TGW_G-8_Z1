7616e6ae45c8c53001763183e5d2663e580980df
RafalWilkowski <kaizen.avc@gmail.com>
2019-11-20 20:07:22 +0100

    Add HUD

---
+++ River Raid/scenes/HUD.gd
@@ -0,0 +1,19 @@
+extends CanvasLayer
+
+onready var lives_panel : Label = $Lives
+onready var fuel_panel : Label = $Fuel
+onready score_panel : Label= $Score
+
+func _ready():
+	connect("lives_left", self, "_on_lives_changed")
+	connect("fuel_left", self, "_on_fuel_changed")
+	connect("fuel_tank_destroyed", self, "_on_score_changed")
+
+func _on_fuel_change(value):
+	fuel_panel.text = "Fuel: " + value
+
+func _on_lives_changed(value):
+	lives_panel.text = "Lives: " + value
+	
+func _on_score_change(value):
+	score_panel.text = "Score: " + value
