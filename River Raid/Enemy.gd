c7fc977aafa66e19c911388bd277d063e85a08ac
RafalWilkowski <kaizen.avc@gmail.com>
2019-11-21 18:48:28 +0100

    tests

--- River Raid/Enemy.gd
+++ River Raid/Enemy.gd
@@ -2,7 +2,13 @@ extends Area2D
 
 signal enemy_destroyed
 
+onready var hud
 export (int) var points = 20
+
+func _ready():
+	hud = get_node("../HUD")
+	print(hud.name)
+	connect("enemy_destroyed", hud,"_on_score_change")
 
 func _on_Enemy_area_entered(area):
 	var area_name = area.get_name()
--- River Raid/scripts/HUD.gd
+++ River Raid/scripts/HUD.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 onready var lives_panel : Label = $Lives
 onready var fuel_panel : Label = $Fuel
 onready var score_panel : Label= $Score
+var score = 0
 
 func _on_fuel_change(value):
 	fuel_panel.text = "Fuel: " + value as String
@@ -11,4 +12,5 @@ func _on_lives_changed(value):
 	lives_panel.text = "Lives: " + value as String
 
 func _on_score_change(value):
-	score_panel.text = "Score: " + value as String
+	score += value
+	score_panel.text = "Score: " + score as String
