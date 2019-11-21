a88045b85c4370879a7ddd4f76872681f339cf2d
pkrywulko <pkrywulko@gmail.com>
2019-11-21 20:00:57 +0100

    added waiting for pressing any key to start flying

--- River Raid/scripts/HUD.gd
+++ River Raid/scripts/HUD.gd
@@ -6,11 +6,11 @@ onready var score_panel : Label= $Score
 var score = 0
 
 func _on_fuel_change(value):
-	fuel_panel.text = "Fuel: " + value as String
+	fuel_panel.text = "Fuel: " + str(value)
 
 func _on_lives_changed(value):
-	lives_panel.text = "Lives: " + value as String
+	lives_panel.text = "Lives: " + str(value)
 
 func _on_score_change(value):
 	score += value
-	score_panel.text = "Score: " + score as String
+	score_panel.text = "Score: " + str(value)
--- River Raid/scripts/Player.gd
+++ River Raid/scripts/Player.gd
@@ -8,45 +8,45 @@ signal fuel_left
 
 export (PackedScene) var Projectile
 export (float) var base_speed = 500
-export (int) var fuel_capacity = 10
+export (int) var fuel_capacity = 64
 export (int) var lives = 3
 
 var projectile = null
-var fuel_amount
+var fuel_amount = fuel_capacity
 var speed
 var screen_size
 var velocity
-var can_fly = false;
+var can_fly = false
+var is_any_button_pressed = false
 
-func _ready():
-	fuel_amount = fuel_capacity
-	screen_size = get_viewport_rect().size
-	position = Vector2(screen_size.x / 2, 960)
+func _input(event):
+	if event is InputEventKey && event.pressed && can_fly && !is_any_button_pressed:
+		is_any_button_pressed = true
+		$FuelTimer.start()
 	
 func _process(delta):
-	if can_fly:
-		move(delta)
+	move(delta)
 	shoot(position)
-	
 	fuel_monitor()
 	live_monitor()
 
 func move(delta):
-	velocity = Vector2()
-	speed = base_speed
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += (1 * base_speed)
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= (1 * base_speed)
-	if Input.is_action_pressed("ui_up"):
-		speed = base_speed * 2
-	if Input.is_action_pressed("ui_down"):
-		speed = base_speed / 2
-
-	velocity.y -= (1 * speed)
-	var pos = position + velocity * delta
-	pos.x = clamp(pos.x, 0, screen_size.x)
-	position = pos
+	if can_fly && is_any_button_pressed:
+		velocity = Vector2()
+		speed = base_speed
+		if Input.is_action_pressed("ui_right"):
+			velocity.x += (1 * base_speed)
+		if Input.is_action_pressed("ui_left"):
+			velocity.x -= (1 * base_speed)
+		if Input.is_action_pressed("ui_up"):
+			speed = base_speed * 2
+		if Input.is_action_pressed("ui_down"):
+			speed = base_speed / 2
+	
+		velocity.y -= (1 * speed)
+		var pos = position + velocity * delta
+		pos.x = clamp(pos.x, 0, screen_size.x)
+		position = pos
 
 func fuel_monitor():
 	emit_signal("fuel_left", fuel_amount)
@@ -60,7 +60,7 @@ func shoot(pos):
 		get_tree().get_root().add_child(projectile)
 
 func can_shoot():
-	return get_tree().get_root().get_node_or_null("Projectile") == null
+	return get_tree().get_root().get_node_or_null("Projectile") == null && is_any_button_pressed
 
 func _on_Player_area_entered(area):
 	var area_name = area.get_name()
@@ -73,18 +73,20 @@ func _on_Player_area_entered(area):
 		print("player_destroyed")
 		can_fly = false
 		emit_signal("player_destroyed")
-		
 		hide()
 
 func _on_FuelTimer_timeout():
-	fuel_amount -= 1
+	if fuel_amount > 0:
+		fuel_amount -= 1
 	if fuel_amount == 0:
 		print("out_of_fuel")
 		emit_signal("out_of_fuel")
 		print("player_destroyed")
 		can_fly = false
+		is_any_button_pressed = false
+		fuel_amount = fuel_capacity
+		$FuelTimer.stop()
 		emit_signal("player_destroyed")
-		
 		hide()
 
 func _on_Player_player_destroyed():
@@ -95,4 +97,6 @@ func _on_Player_player_destroyed():
 		emit_signal("out_of_lives")
 
 func _on_ready_to_go():
-	can_fly = true;
+	screen_size = get_viewport_rect().size
+	position = Vector2(screen_size.x / 2, 960)
+	can_fly = true
