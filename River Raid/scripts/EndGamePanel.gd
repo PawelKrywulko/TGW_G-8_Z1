ebb8428e8b50be541cf61a6f4b81dfdf27e5eeb8
RafalWilkowski <kaizen.avc@gmail.com>
2019-11-26 20:36:39 +0100

    endgamepanel workinprogress

---
+++ River Raid/scripts/EndGamePanel.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_wait()
+
+func _wait():
+	yield(get_tre
+	while true:
+		yield(get_tree(),"idle_frame")
