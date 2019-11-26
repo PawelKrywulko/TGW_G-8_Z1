fe4d687f0128e392158075f08542d571533e14c3
RafalWilkowski <kaizen.avc@gmail.com>
2019-11-26 20:04:27 +0100

    Prepare code to connect/react to end screen/panel

--- River Raid/scripts/GameManager.gd
+++ River Raid/scripts/GameManager.gd
@@ -52,6 +52,9 @@ func reset_game():
 
 func game_running():
 	yield(player, "out_of_lives")
+	#prepere to connect to end panel
+	#yield(endpanel,"gameover")
+	#get_tree().change_scene("res://scenes/StartScreen.tscn")
 	print("game+stoprunning")
 func gameover():
 	gameloop()
