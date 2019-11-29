feac6f6f3cb3b59d5df19f562ef6c4fc12157998
RafalWilkowski <kaizen.avc@gmail.com>
2019-11-29 21:51:20 +0100

    Change end panel logic , clean script

--- River Raid/scripts/EndGamePanel.gd
+++ River Raid/scripts/EndGamePanel.gd
@@ -7,8 +7,11 @@ onready var score_info: Label
 onready var info_message : Label
 
 export var waiting_time : int
+var anykey_pressed: bool
 
 func _ready() -> void:
+	anykey_pressed = false
+	
 	score_info = $ScoreInfo
 	var score = get_parent().get_node("HUD").score
 	score_info.text = "Your final score is : " + str(score)
@@ -17,14 +20,18 @@ func _ready() -> void:
 	info_message.text = "Press any key or wait " + str(waiting_time) + " sec..."
 	
 	timer = $WaitTimer
-	timer.start()
-	
-	wait()
+	timer.start(waiting_time + 1)
 
-func wait() -> void:
-	while true:
-		yield($WaitTimer, "timeout")
-		waiting_time -= 1
+func _input(event) -> void:
+	if event is InputEventKey and event.pressed:
+		anykey_pressed = true
+
+func _process(delta):
+	#delay timer to prevent unwittingly key pressed 
+	yield(get_tree().create_timer(1),"timeout")
+	waiting_time = timer.get_time_left()
+	if waiting_time <= 0 || anykey_pressed:
+		get_tree().change_scene("res://scenes/StartScreen.tscn")
+	else:
 		info_message.text = "Press any key or wait " + str(waiting_time) + " sec..."
-		if waiting_time <= 0 :
-			get_tree().change_scene("res://scenes/StartScreen.tscn")
+
