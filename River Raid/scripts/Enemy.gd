0663974b8e602516583b32c6f01bedb432fc6070
pkrywulko <pkrywulko@gmail.com>
2019-12-04 21:28:27 +0100

    enemy functions on_ready() on_process() and tool mode

--- River Raid/scripts/Enemy.gd
+++ River Raid/scripts/Enemy.gd
@@ -1,28 +1,39 @@
+tool
 extends Area2D
 
 signal enemy_destroyed
 
 export var points: int = 20
-export var startDirection: int = 1 #1 = right ; -1 = left
+export (String, "right", "left") var choose_direction setget set_direction
 export var speed: int = 100
 
-onready var hud = get_node("../HUD")
-onready var game_manager = get_node("../../GameManager")
-onready var raycast = get_node("RayCast2D")
+var start_direction: int
 
-func _ready():
+onready var hud := get_node("../HUD")
+onready var game_manager := get_node("../../GameManager")
+onready var raycast := get_node("RayCast2D")
+
+func _ready() -> void:
+	set_direction(choose_direction)
+	on_ready()
+		
+func on_ready():
 	show()
 	connect("enemy_destroyed", hud, "_on_score_changed")
 	if is_instance_valid(game_manager):
 		game_manager.connect("reset", self, "_on_game_reseted")
-		
-func _process(delta):
+
+func _process(delta: float) -> void:
+	if Engine.editor_hint:
+		return
+	on_process(delta)
+	
+func on_process(delta: float) -> void:
 	if raycast.is_colliding():
 		transform.x *= -1
-		startDirection *= -1
+		start_direction *= -1
 	
-	position.x += startDirection * delta * speed
-
+	position.x += start_direction * delta * speed
 
 func _on_Enemy_area_entered(area):
 	var area_name = area.get_name()
@@ -35,3 +46,14 @@ func _on_Enemy_area_entered(area):
 func _on_game_reseted():
 	show()
 	$CollisionShape2D.set_deferred("disabled", false)
+	
+func set_direction(new_direction) -> void:
+	choose_direction = new_direction
+	if choose_direction == "right":
+		choose_direction = "right"
+		start_direction = 1
+		self.scale.x = 1
+	else:
+		choose_direction = "left"
+		start_direction = -1
+		self.scale.x = -1
