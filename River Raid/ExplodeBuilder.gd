1d2447511bcef2c114a1bfe90333de0f5268c2cc
RafalWilkowski <kaizen.avc@gmail.com>
2019-12-28 19:27:25 +0100

    Add exlodebuilder to enemies

--- River Raid/ExplodeBuilder.gd
+++ River Raid/ExplodeBuilder.gd
@@ -1,19 +1,25 @@
 extends Node2D
 
-export var exlode_scene: PackedScene
+class_name ExplodeBuilder
 
-func _ready():
+export var exlode_scene: PackedScene
+export var explosion_number:= 8
+
+func explode(begin, end):
 	var a = 0
-	while a < 5:
-		yield(get_tree().create_timer(0.1), "timeout")
+	while a < explosion_number:
+		yield(get_tree().create_timer(0.05), "timeout")
 		a += 1
-		var exlode = exlode_scene.instance();
+		var boom = exlode_scene.instance();
 		randomize()
-		exlode.position.x = randi() % 500
-		exlode.position.y = randi() % 500
-		var anim = exlode.get_node("AnimatedSprite") as AnimatedSprite
+		var randX = rand_range(-end.x , end.x)
+		var randY = rand_range(-end.y , end.y)
+		boom.position.x = begin.x + randX
+		boom.position.y = begin.y + randY
+		var anim = boom.get_node("AnimatedSprite") as AnimatedSprite
 		anim.play(str(randi()%2))
-		add_child(exlode)
+		get_parent().add_child(boom)
+	queue_free()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
