28eb1f5825c0826a8ea0d909dc4b6cf630c7357f
RafalWilkowski <kaizen.avc@gmail.com>
2019-12-29 18:27:37 +0100

    Added more flexible sound settings

--- River Raid/scripts/Settings.gd
+++ River Raid/scripts/Settings.gd
@@ -1,22 +1,21 @@
 extends CanvasLayer
 
 func _ready():
+	$MusicSlider.value = SaveSystem.load_value("Music", "Volume")
 	$SoundCheckBox.pressed = SaveSystem.load_value("Music", "MusicEnabled")
-	$VolumeSlider.value = SaveSystem.load_value("Music", "Volume")
-	AudioServer.set_bus_volume_db( 0, $VolumeSlider.value)
+	$SFXSlider.value = SaveSystem.load_value("SFX", "Volume")
 	AudioServer.set_bus_mute( 0, !$SoundCheckBox.pressed)
+	AudioServer.set_bus_volume_db( 1, $MusicSlider.value)
+	AudioServer.set_bus_volume_db( 2, $SFXSlider.value)
 
 func _process(delta):
-	AudioServer.set_bus_volume_db( 0, $VolumeSlider.value)
 	AudioServer.set_bus_mute( 0, !$SoundCheckBox.pressed)
-
-func _on_CloseButton_pressed():
-	queue_free()
-
+	AudioServer.set_bus_volume_db( 1, $MusicSlider.value)
+	AudioServer.set_bus_volume_db( 2, $SFXSlider.value)
+	
 func _on_SaveButton_pressed():
 	SaveSystem.save_value("Music", "MusicEnabled", $SoundCheckBox.pressed)
-	SaveSystem.save_value("Music", "Volume", $VolumeSlider.value)
-	AudioServer.set_bus_volume_db( 0, $VolumeSlider.value)
-	AudioServer.set_bus_mute( 0, !$SoundCheckBox.pressed)
+	SaveSystem.save_value("Music", "Volume", $MusicSlider.value)
+	SaveSystem.save_value("SFX", "Volume", $SFXSlider.value)
 	get_tree().paused = false
 	queue_free()
