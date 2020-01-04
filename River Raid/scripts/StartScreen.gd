cda8f8e8306aca79fc919c9fcfa717ede7742979
RafalWilkowski <kaizen.avc@gmail.com>
2020-01-04 14:00:50 +0100

    Add more clicks!

--- River Raid/scripts/StartScreen.gd
+++ River Raid/scripts/StartScreen.gd
@@ -5,7 +5,10 @@ func _ready():
 	$ButtonsPanel/ButtonQuit.connect("pressed", self, "quit_game")
 
 func start_game():
+	Global.click()
 	get_tree().change_scene("res://scenes/GameManager.tscn")
 
 func quit_game():
+	Global.click()
+	yield(get_tree().create_timer(0.1),"timeout")
 	get_tree().quit()
