e8c1b3c29277e24e89480ac5d530371918c67723
RafalWilkowski <kaizen.avc@gmail.com>
2020-01-05 11:54:39 +0100

    Create config file at first run

--- River Raid/scripts/SaveSystem.gd
+++ River Raid/scripts/SaveSystem.gd
@@ -5,7 +5,10 @@ var config = ConfigFile.new()
 var load_response = config.load(save_path)
 
 func _ready() -> void:
-	load_all_settings()
+	if config.load(save_path) != OK:
+		build_config()
+	else:
+		load_all_settings()
 
 func save_value(section: String, key: String, value) -> void:
 	config.set_value(section, key, value)
@@ -19,3 +22,10 @@ func load_all_settings() -> void:
 	AudioServer.set_bus_mute(2, !SaveSystem.load_value("SFX", "IsEnabled"))
 	AudioServer.set_bus_volume_db(1, SaveSystem.load_value("Music", "Volume"))
 	AudioServer.set_bus_volume_db(2, SaveSystem.load_value("SFX", "Volume"))
+	
+func build_config() -> void:
+	SaveSystem.save_value("Music", "IsEnabled", true)
+	SaveSystem.save_value("SFX", "IsEnabled", true)
+	SaveSystem.save_value("Music", "Volume", 0)
+	SaveSystem.save_value("SFX", "Volume", 0)
+	SaveSystem.save_value("Highscore","Score", 100)
