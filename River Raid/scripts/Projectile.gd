da968cc4f85f3b642ba7f07c3fa260a3fafd2965
RafalWilkowski <kaizen.avc@gmail.com>
2020-01-08 15:11:10 +0100

    Fix projectile destroy

--- River Raid/scripts/Projectile.gd
+++ River Raid/scripts/Projectile.gd
@@ -14,7 +14,7 @@ func _on_Visibility_screen_exited():
 	queue_free()
 
 func _on_Projectile_area_entered(area):
-	if(area.get_name() != "Player"):
+	if !(area.get_name() == "Player" || area.is_in_group("Explosion")):
 		var end_y = $Sprite.get_rect().end.x * $Sprite.scale.x
 		var end_x = $Sprite.get_rect().end.y * $Sprite.scale.y
 		var vector_end = Vector2(end_x, end_y)
